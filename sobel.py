fa72595c9ba6e8c3fc187d80727520d9d53260e7
BZKgenesis <78815908+BZKgenesis@users.noreply.github.com>
2024-09-13 19:40:50 +0200

    GLSL

--- sobel.py
+++ sobel.py
@@ -1,25 +1,39 @@
 from PIL import Image as im
 import numpy as np
+import time
 
 def sobel(image):
-    # Convert the image to grayscale
     image = image.convert('L')
-    # Convert the image to a numpy array
     image = np.array(image)
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            if image[i][j] > 127:
-                image[i][j] = 255
-            else:
-                image[i][j] = 0
+    _img = image.copy()
 
-    # Apply the Sobel filter
-    # Convert the image to a PIL image
-    image = im.fromarray(image)
-    return image
+    XMat = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+    YMat = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
 
+    Gx = np.convolve(np.ndarray.flatten(XMat), np.ndarray.flatten(image))
+    Gy = np.convolve(np.ndarray.flatten(YMat), np.ndarray.flatten(image))
 
-img = im.open('image.png')
+    
+    for i in range(1,image.shape[0]-1):
+        for j in range(1,image.shape[1]-1):
+            for k in range(XMat.shape[0]):
+                for l in range(XMat.shape[1]):
+                    _img[i][j] += XMat[k][l]*image[i+k-1][j+l-1]
 
+    _img = im.fromarray(_img)
+    return _img
+
+img = im.open('1.png')
+start = time.time()
 sobel(img).show()
+end = time.time()
+elapsed = end - start
+print("img 1 : ", elapsed)
+
+img = im.open('2.png')
+start = time.time()
+sobel(img).show()
+end = time.time()
+elapsed = end - start
+print("img 2 : ", elapsed)
 
